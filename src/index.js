e337b1ef5025188992d3e768c9a5b8418ae61b80
maayami <mayank16meena@gmail.com>
2020-10-21 22:11:41 +0530

    basic setup with bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './components/App'
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './Components/App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDom.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
