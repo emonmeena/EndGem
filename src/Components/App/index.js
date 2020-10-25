9a4eb37abcbd2db24343e425168c9ce4214e72c4
maayami <mayank16meena@gmail.com>
2020-10-25 18:33:33 +0530

    Improve appearance

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
-import { Container } from 'reactstrap';
 import axios from 'axios'
 import FilesSection from '../FileSection'
+import './css/index.css'
 
 export default class App extends Component{
     constructor(props){
@@ -11,7 +11,7 @@ export default class App extends Component{
 
     componentDidMount = () =>{
         this.fetchData();
-        this.getCourseMaterial(1);
+        this.getCourseMaterial(0);
     }
 
     fetchData = async () => {
@@ -33,11 +33,30 @@ export default class App extends Component{
         const {courses, files} = this.state;
         return (
             <div>
-                    {
-                        courses.map((course) =>{
-                            return <button key={course.pk} onClick={() => this.getCourseMaterial(course.pk)} >{course.name}</button>
-                        })
-                    }
+                <div className="border bg-light p-3 d-flex justify-content-between">
+                    <div className="d-flex">
+                        <img className="c-logo" src="https://www.iitr.ac.in/departments/CSE/uploads/Misc/IITR_logo/IITR_new_logo_color.jpg" alt="IITR LOGO"/>
+                        <p className="mb-0 pt-2 pl-2 text-secondary h2">EndGem</p>
+                    </div>
+                    <div className="btn-md-group" role="group">
+                        {courses.map((course) =>{
+                            return (
+                            <button type="button" className="btn btn-secondary" key={course.pk} onClick={() => this.getCourseMaterial(course.pk)} >
+                                {course.name}
+                            </button>
+                            )
+                        })}
+                    </div>
+                        <div>
+                            <button className="btn btn-primary">
+                                Add Gem{" "}
+                                <i className="fas fa-plus" />
+                            </button>
+                            <button className="btn btn-secondary mx-2">
+                                <i className="fas fa-bars" />
+                            </button>
+                        </div>
+                </div>
                 <FilesSection files={files} />
             </div>
         );
--- src/Components/FileSection/index.js
+++ src/Components/FileSection/index.js
@@ -1,32 +1,35 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Container } from 'reactstrap';
 
-export default class FileSection extends Component{
+export default function FileSection ({files}){
 
-    render(){
-        return(
-            <Container>
-                {
-                    this.props.files.map(file =>{
-                        return (
-                            <div className="border p-2 rounded bg-light shadow" key={file.pk}>
-                                <div className="d-inline-flex">
-                                    <i className="fas fa-file-pdf fa-3x text-secondary"/>
-                                    <p className="mb-0 pl-3">{file.name}<br/>
-                                        {file.dateAdded}
-                                    </p>
-                                </div>
-                                <div className="float-right my-auto">
-                                        <button type="button" className="btn btn-primary">
-                                            Download <span className="badge badge-light">{file.downloads} </span>
-                                        </button>
-                                </div>
-                               
+    const startMessage = () =>{
+        if(files.length === 0)
+            return <p className="text-secondary display-2 text-center">Select a course to view its Gems <i className="fas fa-gem text-primary"/> !!</p>
+    }
+
+    return(
+            <Container className="mt-3">
+                {startMessage()}
+                { files.map(file =>{
+                    return (
+                        <div className="border m-2 p-2 rounded bg-light shadow" key={file.pk}>
+                            <div className="d-inline-flex">
+                                <i className="fas fa-file-pdf fa-3x text-secondary"/>
+                                <p className="mb-0 pl-3">{file.name}<br/>
+                                    {file.dateAdded}
+                                </p>
                             </div>
+                            <div className="float-right my-auto">
+                                    <button type="button" className="btn btn-primary">
+                                        Download <span className="badge badge-light">{file.downloads} </span>
+                                    </button>
+                            </div>
+                        </div>
                         )
                     })
                 }
             </Container>
         )
-    }
 }
+ 
